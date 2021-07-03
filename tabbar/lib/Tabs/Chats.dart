074d3a05ffb90fc1728d7e2318d82462ab033bc0
Muhammad Osama <mosamarafique@gmail.com>
2021-07-03 10:48:40 +0500

    adding icons to app bar

--- tabbar/lib/Tabs/Chats.dart
+++ tabbar/lib/Tabs/Chats.dart
@@ -47,7 +47,6 @@ class Chats extends StatelessWidget {
             title: Text("Bilal Khan"),
             subtitle: Text("Han mama"),
             leading: CircleAvatar(
-              child: Text("3"),
               backgroundImage: NetworkImage(
                   "https://media-exp1.licdn.com/dms/image/C5622AQFLlvJ57q-Atg/feedshare-shrink_800/0/1623158075642?e=1626307200&v=beta&t=lkwOSq1HLYh52Wh16JVIIJNTZj5UA1Be9P9w6bPSm8M"),
             ),
@@ -73,7 +72,6 @@ class Chats extends StatelessWidget {
               children: [
                 Text("3:25 am"),
                 CircleAvatar(
-                  child: Text("3"),
                   backgroundColor: Colors.greenAccent[200],
                   radius: 10,
                 )
@@ -84,7 +82,6 @@ class Chats extends StatelessWidget {
             title: Text("Gulsher Khan"),
             subtitle: Text("Han mama"),
             leading: CircleAvatar(
-              child: Text("3"),
               backgroundImage: NetworkImage(
                   "https://media-exp1.licdn.com/dms/image/C5622AQFLlvJ57q-Atg/feedshare-shrink_800/0/1623158075642?e=1626307200&v=beta&t=lkwOSq1HLYh52Wh16JVIIJNTZj5UA1Be9P9w6bPSm8M"),
             ),
@@ -92,7 +89,6 @@ class Chats extends StatelessWidget {
               children: [
                 Text("3:25 am"),
                 CircleAvatar(
-                  child: Text("3"),
                   backgroundColor: Colors.greenAccent[200],
                   radius: 10,
                 )
@@ -103,7 +99,6 @@ class Chats extends StatelessWidget {
             title: Text(" Waseem Khan"),
             subtitle: Text("hello "),
             leading: CircleAvatar(
-              child: Text("3"),
               backgroundImage: NetworkImage(
                   "https://media-exp1.licdn.com/dms/image/C5622AQFLlvJ57q-Atg/feedshare-shrink_800/0/1623158075642?e=1626307200&v=beta&t=lkwOSq1HLYh52Wh16JVIIJNTZj5UA1Be9P9w6bPSm8M"),
             ),
--- tabbar/lib/main.dart
+++ tabbar/lib/main.dart
@@ -4,7 +4,9 @@ import 'package:tabbar/Tabs/Camera.dart';
 import 'package:tabbar/Tabs/Chats.dart';
 import 'package:tabbar/Tabs/Status.dart';
 
-void main() {
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+Future<Null> main() async {
   runApp(TabBarDemo());
 }
 
@@ -12,25 +14,33 @@ class TabBarDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      actions: {},
       debugShowCheckedModeBanner: false,
       home: DefaultTabController(
         length: 4,
         child: Scaffold(
           appBar: AppBar(
-            backgroundColor: Colors.teal[900],
-            bottom: TabBar(
-              tabs: [
-                Tab(icon: Icon(Icons.camera_alt_outlined)),
-                Tab(text: "Chats"),
-                Tab(text: "Status"),
-                Tab(
-                  text: "Calls",
+              actions: [
+                IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child:
+                      IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
                 )
               ],
-            ),
-            title: Text('Whatsapp'),
-          ),
+              backgroundColor: Colors.teal[900],
+              bottom: TabBar(
+                tabs: [
+                  Tab(icon: Icon(Icons.camera_alt_outlined)),
+                  Tab(text: "Chats"),
+                  Tab(text: "Status"),
+                  Tab(
+                    text: "Calls",
+                  )
+                ],
+              ),
+              title: Text('Whatsapp'),
+              leading: IconButton(
+                  onPressed: () {}, icon: FaIcon(FontAwesomeIcons.whatsapp))),
           body: TabBarView(
             children: [Camera(), Chats(), Status(), Calls()],
           ),
